951eb631d994df6e61dec8856bbc0d9a94c62353
sagar manghnani <sagarmanghnani@gmail.com>
2018-10-14 12:38:26 +0530

    xo moves are inputted to their respective arrays

--- javascript/xogame.js
+++ javascript/xogame.js
@@ -1,60 +1,81 @@
 //Pubnub object initialization
 var subscribeKey = 'sub-c-d7ac8e5e-c9f3-11e8-80d1-72aadab1d7f1';
 var publishKey = 'pub-c-ef1c9928-afb2-4ecf-bcb3-249c675e31f5';
-var uuid;
+var check;
 var isX = true;
-let movesX = [];
-let movesY = [];
+var movesX = [];
+var movesY = [];
 let occupiedMoves = [];
+var uuid = PubNub.generateUUID();
+console.log(uuid + "uuid");
 var pubnub = new PubNub({
     subscribeKey: subscribeKey,
     publishKey: publishKey,
-    uuid: PubNub.generateUUID(),
     ssl: true,
+    uuid: uuid
+});
 
-})
-;
+var channel = 'xogame16';
 
 //adding listening event
 pubnub.addListener({
-    status:function(statusEvent){
-        if(statusEvent.category === 'PNConnectedCategory'){
-            var newState = {
-                name: 'kuch bhi',
-                timestamp: new Date()
+
+    message: function(m) {
+        console.log(m);
+        movesX = m.message.movesX;
+        movesY = m.message.movesY;
+        occupiedMoves = m.message.occupiedMoves;
+        console.log("isX coming");
+        console.log(isX);
+    },
+    presence: function(presenceEvent) {
+        console.log(isX);
+        check = presenceEvent; 
+        console.log("presenseEvent");
+        console.log(presenceEvent.occupancy );
+        if(presenceEvent.action === 'join' && presenceEvent.uuid === uuid)
+        {
+            if(presenceEvent.occupancy  < 2)
+            {
+                alert("waiting for opponent");
             }
-            pubnub.setState({
-                channels:['xogame'],
-                state:newState
-            })
+            else if(presenceEvent.occupancy  === 2)
+            {
+                isX = false;
+                alert("all players have came");
+            }
+            else if(presenceEvent.occupancy  > 2)
+            {
+                alert("players exceeded");
+                pubnub.unsubscribe({
+                    channels: [channel]
+                })
+            }
         }
     },
 
-    message: function(message) {
-        console.log(message);
-    },
-    presence: function(presenceEvent) {
-        check = presenceEvent;
-        uuid = check.uuid;
-       
-        
+    status: function(event){
+        if(event.category == "PNConnectedCategory")
+        {
+            console.log("connected");
+            createTable();
+        }
     }
 });
 //pubnub subscription
 
-
+pubnub.subscribe({
+    channels: [channel],
+    withPresence: true
+});
 //exit the game
 function exit()
 {
-   pubnub.unsubscribe({
-       channels:['xogame'],
-       callback: function(){
-          
-       }
-   }) 
+   pubnub.unsubscribeAll(); 
 }
 
 //Pubnub message publishing
+
 function publishMessage()
 {
    pubnub.publish(
@@ -64,10 +85,17 @@ function publishMessage()
                movesY:movesY,
                occupiedMoves:occupiedMoves
            },
-           channel: 'xogame'
+           channel: channel
        },
-       function(status, message){
-           console.log(status, message);
+       function(status, response){
+           if(status.error)
+           {
+               console.log(status)
+           }
+           else
+           {
+               console.log("message published ", response.timetoken)
+           }
        }
    )
 }
@@ -86,10 +114,11 @@ function createTable()
             var cell = row.insertCell(j);
             cell.addEventListener('click', function(e){
                 let position = i*3+ j;
-                
+                console.log(movesX);
                 if(isX)
                 {
-                    movesX.push(position);
+                    var l = movesX.push(position);
+                    console.log(movesX);
                 }
                 else
                 {
@@ -98,89 +127,22 @@ function createTable()
                 occupiedMoves.push(position);
                 publishMessage()
             })
+            
             cell.innerHTML = "0";
         }
     }
 }
 
-// Number of Subscribers
-function numSubscriber()
-{
-    pubnub.hereNow({
-        channels: ['xogame']
-    },
-    function (status, response){
-        console.log("coming response");
-        console.log(response);
-        if(response.totalOccupancy < 2)
-        {
-            alert("player is needed to come yet");
-            console.log("response.totalOccupancy = " + response.totalOccupancy)
-        }
 
-        else if(response.totalOccupancy === 2)
-        {
-            isX = false;
-            alert("required players are arrived");
-            console.log("response.totalOccupancy = " + response.totalOccupancy)
-        }
 
-        else{
-            alert("Players exceeded");
-            //exit();
-            console.log("response.totalOccupancy = " + response.totalOccupancy);
-            pubnub.unsubscribe({
-                channels:['xogame']
-            })
-            return;
-        }
-        pubnub.subscribe({
-            channels: ['xogame'],
-            withPresence: true
-        });
-
-    }
-    )
-}
 // it loads createTable when page loads
 window.onload = function () {
-    createTable();
-    numSubscriber();
+    //createTable();
 };
 
 window.addEventListener('close', function(e){
     alert("hello");
 })
 
-
-
-//will be used later
-
-// function Unsubscribe(){
-//     try{
-//         pubnub.unsubscribe({
-//             channels: ['xogame']
-//         });
-//     } catch(err){
-//         console.log("Failed to unsub");
-//     }
-// }
-
-//will be used later
-// function requestToserver()
-// {
-//     Unsubscribe();
-//     var request = new XMLHttpRequest();
-//     request.open('GET', "https://pubsub.pubnub.com/v2/presence/" + subscribeKey + "/channel/" + "xogame/leave?uuid=" + encodeURIComponent(uuid), true);
-//     request.onload = function(){
-//         var data = JSON.parse(this.response);
-//         console.log(data);
-//         alert(data);
-//         alert("hello");
-//     }
-//     alert("hello");
-// }
-
-var check;
 
 
