c8af5775846b598d50b3ccc628acc0f4207687b1
sagar manghnani <sagarmanghnani@gmail.com>
2018-10-15 14:47:13 +0530

    xo winning logic included

--- javascript/xogame.js
+++ javascript/xogame.js
@@ -13,9 +13,12 @@ var pubnub = new PubNub({
     ssl: true,
     uuid: uuid
 });
+const winningCondition = [[0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+                          [0, 3, 6], [1, 4, 7], [2, 5, 8], // colums
+                          [0, 4, 8], [2, 4, 6] ]            // diagonal 
 playedX = false;
 
-var channel = 'xogame14';
+var channel = 'xogame1';
 
 //adding listening event
 pubnub.addListener({
@@ -26,10 +29,19 @@ pubnub.addListener({
         occupiedMoves = m.message.occupiedMoves;
         playedX = m.message.playedX;
         console.log(m.message);
+        // here the functions are mentioned because whenever message is sent and received, functions will be executed at both side when message states changes
         disableMoves(occupiedMoves);
         nameX(movesX);
         nameO(movesY);
         stopCheating();
+        if(winningMoves(movesX))
+        {
+            console.log("X wins");
+        }
+        if(winningMoves(movesY))
+        {
+            console.log("Y wins");
+        }
     },
     presence: function(presenceEvent) {
         check = presenceEvent; 
@@ -222,6 +234,33 @@ function stopCheating()
             tableY.classList.remove("disable");
         }
     }
+}
+
+function winningMoves(winningArray)
+{
+    //idea is winning condition array will sure have the element in the winning array, so if that's condition is true than there is winning else that is not the winning array
+    let winninglength = winningArray.length;
+    let conditionLength = winningCondition.length;
+    let counter = 0;
+    winningArray = winningArray.sort();
+    //looping through winning condition to find the first element of winning condition in winning array
+   if(winninglength >= 3)
+   {
+        for(let i = 0;i<conditionLength;i++)
+        {
+            let firstElementOfWinning = winningCondition[i][0];
+            if(winningArray.indexOf(firstElementOfWinning) >= 0)
+            {
+                if(winningCondition[i].every((val) => {
+                    return winningArray.indexOf(val) >= 0;
+                })){
+                    return true;
+                }
+            }
+        }
+        return false;
+   }
+    
 }
 
 // it loads createTable when page loads
